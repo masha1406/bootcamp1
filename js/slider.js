986eae6c170e5c537da7947a95a0d71a72151e8e
masha1406 <m1krugliak4@gmail.com>
2020-07-13 14:43:04 +0300

    fix styles for carousel

--- js/slider.js
+++ js/slider.js
@@ -1,67 +1,298 @@
+
 "use strict";
+const Ant = function (crslId) {
+  let id = document.getElementById(crslId);
+  if (id) {
+    this.crslRoot = id;
+  } else {
+    this.crslRoot = document.querySelector(".ant-carousel");
+  }
+  // все объекты нашего слайдера
+  // list
+  this.crslList = this.crslRoot.querySelector(".ant-carousel-list");
+  this.crslElements = this.crslRoot.querySelectorAll(".ant-carousel-element");
+  this.crslElemFirst = this.crslRoot.querySelector(".ant-carousel-element");
+  // left_arrow
+  this.leftArrow = this.crslRoot.querySelector("div.ant-carousel-arrow-left");
+  this.rightArrow = this.crslRoot.querySelector("div.ant-carousel-arrow-right");
+  this.indicatorDots = this.crslRoot.querySelector(".ant-carousel-dots");
 
-const Ant=function(carouselId){
-    let carouselId = document.getElementById(carouselId);
-    if (carouselId){
-        this.carouselRoot=carouselId;    
-    } else {
-        this.carouselRoot=document.querySelector(".ant-carousel");
-    }
-    // все объекты нашего слайдера
-    this.list=this.carouselRoot.querySelector(".ant-carousel-list");
-    this.items=this.carouselRoot.querySelectorAll(".ant-carousel-element");
-    this.firstItem=this.carouselRoot.querySelector("ant-carousel-element");
-    this.left_arrow=this.carouselRoot.querySelector(".ant-carousel-arrow-left");
-    this.right_arrow=this.carouselRoot.querySelector(".ant-carousel-arrow-right");
-    this.dots=this.carouselRoot.querySelector(".ant-carousel-dots");
+  // запуск слайдера
+  this.options = Ant.defaults;
+  Ant.initialize(this);
 };
-Ant.defaults={
-    visibleItem: 1,
-    loop: true,
-    auto: true,
-    interval: 2000,
-    speed: 1500,
-    touch: true,
-    arrows: true,
-    dots: true,
+
+Ant.defaults = {
+  // Default options for the carousel
+
+  // visibleItem
+  elemVisible: 1, 
+  loop: true, 
+  auto: true, 
+  interval: 2500, 
+  speed: 1250, 
+  interval: 3000, 
+  speed: 1000, 
+  touch: true, 
+  arrows: true, 
+  dots: true, 
 };
-Ant.prototype.elementPrev=function(num){
-    num=num || 1;
-
-    if (this.options.dots) this.dotOn(this.currentElement);
-    this.currentElement-=num;
-    if (this.currentElement<0) this.currentElement=this.visibleItem-1;
-    if (this.options.dots) this.dotOff(this.currentElement);
-        
-    if (!this.options.loop) {
-        this.currentOffset += this.elemWidth *num;
-        this.list.style.marginLeft = this.currentOffset + "px";
-        if (this.currentElement == 0) {
-            this.left_arrow.style.display = "none";
-            this.touchPrev=false;
-        }
-        this.right_arrow.style.display = "block";
-        this.touchNext=true;
-    } else {
-        let elem, buf, this$ = this;
-
-        for (let i=0; i<num; i++) {
-            // записываем ссылку на последний елемент списка в переменную elem
-            elem=this.list.lastElementChild;
-            // создаем глубокий (полный) клон последнего елемента списка
-            buf=elem.cloneNode(true);
-            // встраиваем склонированый последний елемент списка в самое начало списка
-            this.list.insertBefore(buf, this.list.firstElementChild);
-            // удаляем последний елемент списка из которого делали клон
-            elem.remove();
-        }
-        this.list.style.cssText = "transition:margin " + this.options.speed + "ms ease;";
-        this.list.style.marginLeft = "0px";
-        setTimeout(() => {
-            this$.list.style.cssText = "transition:none";
-        }, this.options.speed);
+Ant.prototype.elemPrev = function (num) {
+  num = num || 1;
+  if (this.options.dots) this.dotOn(this.currentElement);
+  this.currentElement -= num;
+  if (this.currentElement < 0) this.currentElement = this.dotsVisible - 1;
+  if (this.options.dots) this.dotOff(this.currentElement);
+  if (!this.options.loop) {
+    // сдвиг вправо без цикла
+    this.currentOffset += this.elemWidth * num;
+    this.crslList.style.marginLeft = this.currentOffset + "px";
+    if (this.currentElement == 0) {
+      this.leftArrow.style.display = "none";
+      this.touchPrev = false;
     }
-}
-Ant.prototype.elementNext=function(num){
-    
-}
+    this.rightArrow.style.display = "block";
+    this.touchNext = true;
+  } else {
+    // сдвиг вправо с циклом
+    let elm,
+      buf,
+      this$ = this;
+    for (let i = 0; i < num; i++) {
+      elm = this.crslList.lastElementChild;
+      buf = elm.cloneNode(true);
+      this.crslList.insertBefore(buf, this.crslList.firstElementChild);
+      elm.remove();
+    }
+    // this.crslList.style.marginLeft = "-" + this.elemWidth * num + "px";
+    // let compStyle = window.getComputedStyle(this.crslList).marginLeft;
+    this.crslList.style.cssText =
+      "transition:margin " + this.options.speed + "ms ease;";
+    this.crslList.style.marginLeft = "0px";
+    // this.crslList.style.marginLeft = "0px";
+    setTimeout(function () {
+      this$.crslList.style.cssText = "transition:none;";
+    }, this.options.speed);
+  }
+};
+
+// в видео 04 06
+Ant.prototype.elemNext = function (num) {
+  num = num || 1;
+
+  if (this.options.dots) this.dotOn(this.currentElement);
+  this.currentElement += num;
+  if (this.currentElement >= this.dotsVisible) this.currentElement = 0;
+  if (this.options.dots) this.dotOff(this.currentElement);
+
+  if (!this.options.loop) {
+    // сдвиг влево без цикла
+    this.currentOffset -= this.elemWidth * num;
+    this.crslList.style.marginLeft = this.currentOffset + "px";
+    if (this.currentElement == this.dotsVisible - 1) {
+      this.rightArrow.style.display = "none";
+      this.touchNext = false;
+    }
+    this.leftArrow.style.display = "block";
+    this.touchPrev = true;
+  } else {
+    // сдвиг влево с циклом
+    let elm,
+      buf,
+      this$ = this;
+    this.crslList.style.cssText =
+      "transition:margin " + this.options.speed + "ms ease;";
+    this.crslList.style.marginLeft = "-" + this.elemWidth * num + "px";
+    setTimeout(function () {
+      this$.crslList.style.cssText = "transition:none;";
+      for (let i = 0; i < num; i++) {
+        elm = this$.crslList.firstElementChild;
+        buf = elm.cloneNode(true);
+        this$.crslList.appendChild(buf);
+        this$.crslList.removeChild(elm);
+      }
+      this$.crslList.style.marginLeft = "0px";
+    }, this.options.speed);
+  }
+};
+
+Ant.prototype.dotOn = function (num) {
+  this.indicatorDotsAll[num].style.cssText =
+    "background-color:#BBB; cursor:pointer;";
+};
+
+Ant.prototype.dotOff = function (num) {
+  this.indicatorDotsAll[num].style.cssText =
+    "background-color:#556; cursor:default;";
+};
+
+
+Ant.initialize = function (that) {
+  // Constants
+  that.elemCount = that.crslElements.length;
+  that.dotsVisible = that.elemCount; 
+  let elemStyle = window.getComputedStyle(that.crslElemFirst);
+  that.elemWidth =
+    that.crslElemFirst.offsetWidth + 
+    parseInt(elemStyle.marginLeft) +
+    parseInt(elemStyle.marginRight);
+
+  // Variables
+  that.currentElement = 0;
+  that.currentOffset = 0;
+  that.touchPrev = true;
+  that.touchNext = true;
+  let xTouch, yTouch, xDiff, yDiff, stTime, mvTime;
+  let bgTime = getTime();
+  // console.log(bgTime);
+
+  // Functions
+  function getTime() {
+    return new Date().getTime();
+  }
+
+  function setAutoScroll() {
+    that.autoScroll = setInterval(function () {
+      let fnTime = getTime();
+      if (fnTime - bgTime + 20 > that.options.interval) {
+        bgTime = fnTime;
+        that.elemNext();
+      }
+    }, that.options.interval);
+  }
+
+  // Start initialization
+  if (that.elemCount <= that.options.elemVisible) {
+    // Отключить навигацию
+    that.options.auto = false;
+    that.options.touch = false;
+    that.options.arrows = false;
+    that.options.dots = false;
+    that.leftArrow.style.display = "none";
+    that.rightArrow.style.display = "none";
+  }
+
+  if (!that.options.loop) {
+    // если нет цикла - уточнить количество точек
+    that.dotsVisible = that.elemCount - that.options.elemVisible + 1;
+    that.leftArrow.style.display = "none"; // отключить левую стрелку
+    that.touchPrev = false; // отключить прокрутку прикосновением вправо
+    that.options.auto = false; // отключить автопркрутку
+  } else if (that.options.auto) {
+    // инициализация автопрокруки
+    setAutoScroll();
+    // Остановка прокрутки при наведении мыши на элемент
+    that.crslList.addEventListener(
+      "mouseenter",
+      function () {
+        clearInterval(that.autoScroll);
+      },
+      false
+    );
+    that.crslList.addEventListener("mouseleave", setAutoScroll, false);
+  }
+
+  if (that.options.touch) {
+    // инициализация прокрутки прикосновением
+    that.crslList.addEventListener(
+      "touchstart",
+      function (e) {
+        xTouch = parseInt(e.touches[0].clientX);
+        yTouch = parseInt(e.touches[0].clientY);
+        stTime = getTime();
+      },
+      false
+    );
+    that.crslList.addEventListener(
+      "touchmove",
+      function (e) {
+        if (!xTouch || !yTouch) return;
+        xDiff = xTouch - parseInt(e.touches[0].clientX);
+        yDiff = yTouch - parseInt(e.touches[0].clientY);
+        mvTime = getTime();
+        if (
+          Math.abs(xDiff) > 15 &&
+          Math.abs(xDiff) > Math.abs(yDiff) &&
+          mvTime - stTime < 75
+        ) {
+          stTime = 0;
+          if (that.touchNext && xDiff > 0) {
+            bgTime = mvTime;
+            that.elemNext();
+          } else if (that.touchPrev && xDiff < 0) {
+            bgTime = mvTime;
+            that.elemPrev();
+          }
+        }
+      },
+      false
+    );
+  }
+  
+  if (that.options.arrows) {
+    // инициализация стрелок
+    if (!that.options.loop)
+      that.crslList.style.cssText =
+        "transition:margin " + that.options.speed + "ms ease;";
+    that.leftArrow.addEventListener(
+      "click",
+      function () {
+        let fnTime = getTime();
+        if (fnTime - bgTime > that.options.speed) {
+          bgTime = fnTime;
+          that.elemPrev();
+        }
+      },
+      false
+    );
+    that.rightArrow.addEventListener(
+      "click",
+      function () {
+        let fnTime = getTime();
+        if (fnTime - bgTime > that.options.speed) {
+          bgTime = fnTime;
+          that.elemNext();
+        }
+      },
+      false
+    );
+  } else {
+    that.leftArrow.style.display = "none";
+    that.rightArrow.style.display = "none";
+  }
+
+  if (that.options.dots) {
+    // инициализация индикаторных точек
+    let sum = "",
+      diffNum;
+    for (let i = 0; i < that.dotsVisible; i++) {
+      sum += '<span class="ant-dot"></span>';
+    }
+    that.indicatorDots.innerHTML = sum;
+    that.indicatorDotsAll = that.crslRoot.querySelectorAll("span.ant-dot");
+    // Назначаем точкам обработчик события 'click'
+    for (let n = 0; n < that.dotsVisible; n++) {
+      that.indicatorDotsAll[n].addEventListener(
+        "click",
+        function () {
+          diffNum = Math.abs(n - that.currentElement);
+          if (n < that.currentElement) {
+            bgTime = getTime();
+            that.elemPrev(diffNum);
+          } else if (n > that.currentElement) {
+            bgTime = getTime();
+            that.elemNext(diffNum);
+          }
+          // Если n == that.currentElement ничего не делаем
+        },
+        false
+      );
+    }
+    that.dotOff(0); // точка[0] выключена, остальные включены
+    for (let i = 1; i < that.dotsVisible; i++) {
+      that.dotOn(i);
+    }
+  }
+};
+
+new Ant();
